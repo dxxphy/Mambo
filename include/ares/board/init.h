b0c3b41c4fbad6790c50a60db92788e119f8fd17
ttwards <12411711@mail.sustech.edu.cn>
2026-07-04 17:45:06 +0800

    feat(board): add DM-MC02 XT30 regulator control
    Model the two XT30 outputs as fixed regulators that boot off by default and expose board helpers for explicit application control.

--- include/ares/board/init.h
+++ include/ares/board/init.h
@@ -2,9 +2,31 @@
 #ifndef INIT_H
 #define INIT_H
 
+#include <stdbool.h>
+
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/led.h>
+#include <zephyr/sys/util.h>
 
 struct led_rgb;
+
+#ifdef CONFIG_BOARD_DM_MC02
+enum ares_board_xt30 {
+	ARES_BOARD_XT30_1 = BIT(0),
+	ARES_BOARD_XT30_2 = BIT(1),
+	ARES_BOARD_XT30_ALL = ARES_BOARD_XT30_1 | ARES_BOARD_XT30_2,
+};
+
+int ares_board_xt30_power_set(enum ares_board_xt30 xt30, bool enable);
+static inline int ares_board_xt30_power_on(enum ares_board_xt30 xt30)
+{
+	return ares_board_xt30_power_set(xt30, true);
+}
+
+static inline int ares_board_xt30_power_off(enum ares_board_xt30 xt30)
+{
+	return ares_board_xt30_power_set(xt30, false);
+}
+#endif /* CONFIG_BOARD_DM_MC02 */
 
 #endif /* INIT_H */
